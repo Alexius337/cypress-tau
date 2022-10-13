b7ca6aad2d7724d5673b5bb9a7dde7b2894c35bb
Alexius337 <almyro@gmail.com>
2022-10-12 23:22:42 -0700

    write first end to end todo test

--- cypress/e2e/todomvc.cy.js
+++ cypress/e2e/todomvc.cy.js
@@ -1,5 +1,16 @@
 /// <reference types="cypress" />
 
-it('should navigate to the TodoMVC App', () => {
+it('should be able to add new todo to the list', () => {
+    // navigate to the application address
     cy.visit('http://todomvc-app-for-testing.surge.sh')
+
+    // select "new todo" box and type "Clean room" and press enter
+    // increase the default timeout to 6 seconds
+    cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
+
+    // select "toggle" next to todo item and click it to complete todo
+    cy.get('.toggle').click()
+
+    // select element that contains the text "Clear completed" and click on it
+    cy.contains('Clear completed').click()
 })
