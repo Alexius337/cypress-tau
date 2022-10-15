390d8ae15a7bef76c288130929a80a86abcd29e3
Alexius337 <almyro@gmail.com>
2022-10-14 21:29:41 -0700

    add validation to each of our test steps

--- cypress/e2e/todomvc.cy.js
+++ cypress/e2e/todomvc.cy.js
@@ -8,9 +8,21 @@ it('should be able to add new todo to the list', () => {
     // increase the default timeout to 6 seconds
     cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
 
+    // validate that label contains that text we just put in
+    cy.get('label').should('have.text', 'Clean room')
+
+    // assert that toggle is not checked yet
+    cy.get('.toggle').should('not.be.checked')
+
     // select "toggle" next to todo item and click it to complete todo
     cy.get('.toggle').click()
 
+    // assert that toggling todo puta line through the text
+    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+
     // select element that contains the text "Clear completed" and click on it
     cy.contains('Clear completed').click()
+
+    // assert that there are no li elements under the ul element
+    cy.get('.todo-list').should('not.have.descendants', 'li')
 })
